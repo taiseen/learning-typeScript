9c94afb4a8b6a4c2eb0de2d18e9c6a0c37d7f3b8
taiseen <taiseen.cse@gmail.com>
2023-05-02 22:06:17 +0600

    feat: multiple data type array

--- src/script/js/lws/index.js
+++ src/script/js/lws/index.js
@@ -1,3 +1,6 @@
 "use strict";
 const lws = 'Learn With Sumit';
 console.log(lws);
+const data = [];
+data.push('Taiseen', 1);
+console.log(data);
--- src/script/ts/lws/index.ts
+++ src/script/ts/lws/index.ts
@@ -1,2 +1,7 @@
 const lws = 'Learn With Sumit';
 console.log(lws);
+
+const data: (string | number)[] = [];
+
+data.push('Taiseen', 1);
+console.log(data);
